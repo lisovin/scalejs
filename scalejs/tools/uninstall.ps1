4ba9e30949a19f55fbcdc9e1a18f6ababa5e672b
Peter Lisovin <peter@lisovin.com>
2013-09-13 16:23:10 -0400

    Append a version to js file only when added to nuget package.

--- scalejs/tools/uninstall.ps1
+++ scalejs/tools/uninstall.ps1
@@ -2,7 +2,7 @@
 
 $project | 
 	Remove-Import '$(SolutionDir)\.scalejs\Scalejs.targets' |
-	Remove-Paths 'scalejs, es5-shim' |
+	Remove-Paths 'scalejs' |
 	Out-Null
 
 if (Test-Path "$(Get-SolutionDir)\.scalejs") {
