48b35e1188d8ae255c8147d414386b391b19bf44
egucciar <egucciar@gmail.com>
2013-12-12 15:41:46 -0500

    Removed all files from branch

--- scalejs/tools/init.ps1
+++
@@ -1,6 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-$solutionDir = Split-Path $dte.Solution.Properties.Item("Path").Value
-$scalejsPS = "$solutionDir\.scalejs\Scalejs.psd1"
-if (Test-Path $scalejsPS) {
-	Import-Module $scalejsPS
-}
