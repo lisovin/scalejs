4805185eee936d3e03a225bdf0dc1fec1e16a0c8
nicholasantonov <nicholasantonov@gmail.com>
2014-07-28 05:27:33 -0400

    Update embedded application

--- scalejs-docs/src/files/autocomplete/autocomplete-test-1.0.1.js
+++ scalejs-docs/src/files/autocomplete/autocomplete-test-1.0.1.js
@@ -11159,28 +11159,18 @@ define('scalejs.mvvm',[
 });
 
 
-/*global define, console, document*/
-define('scalejs.autocomplete-select2/autocomplete',[
-    'scalejs!core',
-    'knockout',
-    'jQuery',
-    'select2',
-    'scalejs.mvvm'
+/*global define, console*/
+define('map-items',[
+    'scalejs!core'
 ], function (
-    core,
-    ko,
-    $
+    core
 ) {
     
 
     var // Imports
-        observable = ko.observable,
-        computed = ko.computed,
-        unwrap = ko.unwrap,
-        isObservable = ko.isObservable,
-        merge = core.object.merge,
         is = core.type.is;
 
+    // Gets the property for the current level from a path.
     function getProperty(path) {
         if (is(path, 'string')) {
             return path;
@@ -11191,20 +11181,29 @@ define('scalejs.autocomplete-select2/autocomplete',[
         return undefined;
     }
 
-    // Get the childpath for the next level of heiarchical data
+    // Change the path so that it is correct for the next level of data.
     function getNextProperty(path) {
+        var newPath;
+
         if (is(path, 'string')) {
             return path;
         }
         if (is(path, 'array') && path.length > 1) {
-            return path.shift(1);
+            newPath = path.slice();
+            // give tail of array as the path for next level of data
+            newPath.shift(1);
+
+            return (newPath.length === 1)
+                    ? newPath[0]
+                    : newPath;
         }
+        console.error('malformed data when advancing property', path);
         return undefined;
     }
 
     // Take an array and return an array compatible with select2
-    function mapArray(array, idPath, textPath, childPath, selectGroupNodes) {
-        return array.map(function (d) {
+    function mapItems(items, idPath, textPath, childPath, selectGroupNodes) {
+        return items.map(function (item) {
             var children,
                 id,
                 text,
@@ -11214,41 +11213,171 @@ define('scalejs.autocomplete-select2/autocomplete',[
 
             // ----Proccess text field----
             if (currentTextPath) {
-                text = d[currentTextPath];
-            } else if (is(d, 'string')) {
-                text = d;
-            } else { //TODO add isformatted boolean and base this off that
-                console.warn('Input has not specified text field');
+                text = item[currentTextPath];
+            } else if (is(item, 'string')) {
+                text = item;
+            } else {// Not fatal since formatting will make this field useless
                 text = "No Text Specified";
             }
 
             // ----Deal with nodes with children----
-            if (d.hasOwnProperty(currentChildPath)) {
-                children = mapArray(d[currentChildPath], getNextProperty(idPath), getNextProperty(textPath), getNextProperty(childPath), selectGroupNodes);
+            if (item.hasOwnProperty(currentChildPath)) {
+                children = mapItems(item[currentChildPath], getNextProperty(idPath), getNextProperty(textPath), getNextProperty(childPath), selectGroupNodes);
                 if (!selectGroupNodes) {
-                    return { text: text, children: children };
+                    return { text: text, children: children, original: item };
                 }
             }
 
-            // ----Deal with object nodes----
-            id = currentIDPath ? d[currentIDPath] : d;
+            // ----Deal with selectable nodes----
+            id = currentIDPath ? item[currentIDPath] : item;
+
             if (selectGroupNodes) {
-                return { text: text, id: id, children: children };
+                return { text: text, id: id, children: children, original: item };
             }
-            return { text: text, id: id };
-            
+            return { text: text, id: id, original: item };
         });
     }
 
-    function initializeSelect2(element, valueAccessor) {
+    return mapItems;
+});
+/*global define, console, document*/
+define('template',[
+    'knockout',
+    'jQuery'
+], function (
+    ko,
+    $
+) {
+    
+
+    var // Imports
+        cleanNode = ko.cleanNode,
+        applyBindings = ko.applyBindings,
+        // Variables
+        dummyDiv;
+
+    // Create div to render templates inside to get the html to pass to select2, then hide it
+    function createDummyDiv() {
+        if (dummyDiv === undefined) {
+            dummyDiv = document.createElement('div');
+            $(dummyDiv).hide();
+            dummyDiv.setAttribute("data-bind", "template: { name: template, data: data }");
+        }
+    }
+
+    function createFormatFunction(templateString) {
+        createDummyDiv();
+        return function (d) {
+
+            cleanNode(dummyDiv);
+
+            // Clear Dummy Div html node
+            while (dummyDiv.firstChild) {
+                dummyDiv.removeChild(dummyDiv.firstChild);
+            }
+
+            // render template with (d)
+            applyBindings({ template: templateString, data: d.original }, dummyDiv);
+
+            // give rendered data to select2
+            return dummyDiv.innerHTML;
+        };
+    }
+
+    return createFormatFunction;
+});
+/*global define, console*/
+define('query',[
+    'scalejs!core',
+    'knockout',
+    'map-items'
+], function (
+    core,
+    ko,
+    mapItems
+) {
+    
+
+    var // Imports
+        computed = ko.computed,
+        is = core.type.is,
+        // Variables
+        data,
+        queryComputed;
+
+    function createQueryFunction(itemsSource, idpath, textpath, childpath, selectGroupNodes) {
+        return function (query) {
+            if (queryComputed) {
+                queryComputed.dispose();
+            }
+            queryComputed = computed(function () {
+                data = { results: mapItems(itemsSource(), idpath, textpath, childpath, selectGroupNodes) };
+                if (!is(data.results, 'array')) {
+                    console.warn('itemsToShow must return an array');
+                    data.results = [];
+                }
+                query.callback(data);
+            });
+        };
+    }
+
+    return createQueryFunction;
+});
+/*global define, console, document*/
+define('scalejs.autocomplete-select2/autocomplete',[
+    'knockout',
+    'map-items',
+    'template',
+    'query',
+    'jQuery',
+    'select2',
+    'scalejs.mvvm'
+], function (
+    ko,
+    mapItems,
+    createFormatFunction,
+    createQueryFunction,
+    $
+) {
+    
+
+    var // Imports
+        isObservable = ko.isObservable;
+
+    function subscribeToSelectedItem(selectedItem, element) {
+        if (isObservable(selectedItem)) {
+            $(element).on("change", function (o) {
+                selectedItem(o.val);
+            });
+        } else {
+            console.error('selectedItem must be an observable');
+        }
+    }
+
+    function subscribeToUserInput(userInput, element) {
+        var container = $(element).select2("container"),
+            input = $(container).find(".select2-drop .select2-search .select2-input");
+
+        if (isObservable(userInput)) {
+            // Push the user input to the viewmodel
+            $(input).on("keyup", function () {
+                userInput($(input).val());
+            });
+
+            // Make sure that the last user input repopulates the input box when reopened
+            $(element).on("select2-open", function () {
+                $(input).val(userInput());
+            });
+        } else {
+            console.error('userInput must be an observable');
+        }
+    }
+
+    function init(element, valueAccessor) {
 
         var // Scope variables
             value = valueAccessor(),
             select2 = value.select2,
-            userIsHandlingFormatting,
-            createFormatFunction,
-            container,
-            input,
             // Important Values from accessor
             itemsSource =           value.itemsSource,
             itemTemplate =          value.itemTemplate,
@@ -11259,68 +11388,40 @@ define('scalejs.autocomplete-select2/autocomplete',[
             userInput =             value.queryText,
             selectedItem =          value.selectedItem,
             selectGroupNodes =      value.selectGroupNodes,
-            customFiltering =       value.customFiltering,
-            // Temporary variables
-            dummyDiv,
-            queryComputed;
+            customFiltering =       value.customFiltering;
 
         // ----Set up object to pass to select2 with all it's configuration properties----
-        if (select2 === undefined) {
+        if (select2 === undefined || select2 === null) {
             select2 = {};
         }
-        // If they passed itemsToShow, display all of them, else let select2 handle the search
+
+        // If customFiltering is enabled, display all of them, else let select2 handle the search
         if (customFiltering) {
-            select2.query = function (query) {
-                if (queryComputed) {
-                    queryComputed.dispose();
-                }
-                queryComputed = computed(function () {
-                    data = { results: mapArray(itemsSource(), idpath, textpath, childpath, selectGroupNodes) };
-                    if (!is(data.results, 'array')) {
-                        console.warn('itemsToShow must return an array');
-                        data.results = [];
-                    }
-                    query.callback(data);
-                });
-            };
+            select2.query = createQueryFunction(itemsSource, idpath, textpath, childpath, selectGroupNodes);
         } else {
             if (isObservable(itemsSource)) {
+                // Select2 will execute a function passed as a data paramater, and this is the best way to push data through an observable to select2
                 select2.data = function () {
-                    var results = mapArray(itemsSource(), idpath, textpath, childpath, selectGroupNodes);
-                    return { results: results };
+                    var results = mapItems(itemsSource(), idpath, textpath, childpath, selectGroupNodes);
+                    return { results: results }; // this has to be an object due to this being an undocumented select2 feature
                 };
             } else if (itemsSource) {// its just a plain array
-                select2.data = mapArray(itemsSource, idpath, textpath, childpath, selectGroupNodes);
+                select2.data = mapItems(itemsSource, idpath, textpath, childpath, selectGroupNodes);
             }
         }
 
         // ----handle templating----
         if (itemTemplate) {
 
-            // Create div to render templates inside to get the html to pass to select2
-            $('body').append('<div id="dummy_div" data-bind="template: { name: template, data: data }"></div>');
-            dummyDiv = document.getElementById("dummy_div");
-            $(dummyDiv).hide();
-
-            createFormatFunction = function (templateString) {
-                return function (d) {
-                    ko.cleanNode(dummyDiv);
-                    // Clear Dummy Div html node
-                    dummyDiv.innerText = '';
-                    // render template with (d)
-                    ko.applyBindings({ template: templateString, data: idpath ? d : d.id }, dummyDiv);
-
-                    // give rendered data to select2
-                    return dummyDiv.innerHTML;
-                };
-            };
-
+            // Make select2 apply this template to all items
             select2.formatResult = createFormatFunction(itemTemplate);
             select2.formatSelection = select2.formatResult;
 
+            // If the user gave a more specific template for seletcted item, use that instead
             if (selectedItemTemplate) {
                 select2.formatSelection = createFormatFunction(selectedItemTemplate);
             }
+            // This function is run on the data, and by default removes HTML, so we override it to render our templated HTML
             if (!select2.hasOwnProperty('escapeMarkup')) {
                 select2.escapeMarkup = function (m) { return m; };
             }
@@ -11329,36 +11430,20 @@ define('scalejs.autocomplete-select2/autocomplete',[
         // Pass all the set up properties to the select2 constructor and instantiate the select2 box
         $(element).select2(select2);
 
-        // Make sure knockout updates correctly
-        $(element).on("change", function (o) {
-            selectedItem(o.val);
-        });
+        // Push item selections to viewmodel
+        if (selectedItem) {
+            subscribeToSelectedItem(selectedItem, element);
+        }
 
         // ----Handle the user text input----
-
-        container = $(element).select2("container");
-        input = $(container).find(".select2-drop .select2-search .select2-input");
-
         if (userInput) {
-            // Push the user input to the viewmodel
-            $(input).on("keyup", function () {
-                userInput($(input).val());
-            });
-
-            // Make sure that the last user input repopulates the input box when reopened
-            $(element).on("select2-open", function () {
-                $(input).val(userInput());
-            });
+            subscribeToUserInput(userInput, element);
         }
 
         // ----Set up the disposal of select2----
         ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
             $(element).select2('destroy');
         });
-    }
-
-    function init(element, valueAccessor, allBindingsAccessor, viewModel) {
-        initializeSelect2(element, valueAccessor, allBindingsAccessor, viewModel);
     }
 
     return {
@@ -14758,7 +14843,7 @@ define('app/main/bindings/mainBindings',{
     'auto': function () {
         return {
             autocomplete: {
-                itemsSource: ['Nick', 'Conor', 'Nissam', 'Serge', 'Jeremy', 'Peter', 'Ernie', 'Bruce', 'Vlad', 'Mike'],
+                itemsSource: ['Nick', 'Conor', 'Nissim', 'Dan', 'Serge', 'Dave',  'Jeremy', 'Peter', 'Ernie', 'Bruce', 'Vlad', 'Mike'],
                 selectedItem: this.selectedItem,
             }
         }
@@ -15057,7 +15142,7 @@ define('app/main/mainModule',[
 });
 
 
-define("scalejs/extensions", ["scalejs.autocomplete-select2","scalejs.autocomplete-select2","scalejs.functional","scalejs.functional","scalejs.linq-linqjs","scalejs.mvvm","scalejs.mvvm","scalejs.statechart-scion"], function () { return Array.prototype.slice(arguments); });
+define("scalejs/extensions", ["scalejs.autocomplete-select2","scalejs.functional","scalejs.functional","scalejs.linq-linqjs","scalejs.mvvm","scalejs.mvvm","scalejs.statechart-scion"], function () { return Array.prototype.slice(arguments); });
 /*global require*/
 require([
     'scalejs!application/main'
